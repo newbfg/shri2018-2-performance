9fcdb0b9c5b90f6e15d66a705b1d54c36415914e
Telegin <telegin.zhenya@gmail.com>
2018-10-20 12:07:50 +0300

    test(webpack.prod.js): change to dev environment

--- webpack.prod.js
+++ webpack.prod.js
@@ -4,24 +4,10 @@ const common = require('./webpack.common.js');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 
-module.exports = merge.smart(
-  common,
-  // {
-  //   optimization: {
-  //     minimizer: [
-  //       new UglifyJsPlugin({
-  //         cache: true,
-  //         parallel: true
-  //       }),
-  //       new OptimizeCSSAssetsPlugin({})
-  //     ]
-  //   },
-  //   mode: 'production'
-  // },
-  {
-    output: {
-      path: path.resolve(__dirname, 'dist'),
-      publicPath: './'
-    }
+module.exports = merge.smart(common, {
+  mode: 'development',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: './'
   }
-);
+});
