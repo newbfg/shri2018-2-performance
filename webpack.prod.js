41af8cbf56ae57621cc8079026b082ef8ec9efb6
Telegin <telegin.zhenya@gmail.com>
2018-10-26 20:39:19 +0300

    feat: add css minification

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,11 +1,15 @@
 const merge = require('webpack-merge');
 const path = require('path');
 const common = require('./webpack.common.js');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 
 module.exports = merge.smart(
   common,
   {
-    mode: 'production'
+    mode: 'production',
+    optimization: {
+      minimizer: [new OptimizeCSSAssetsPlugin({})]
+    }
   },
   {
     output: {
