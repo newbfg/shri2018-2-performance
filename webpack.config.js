c60308c140ffcf6d2d69b58ecf4ae2b022ce48af
Илья <Ilja@MacBook-Pro-Ila.Dlink>
2018-10-30 22:33:46 +0300

    chore(code): crete bundle

---
+++ webpack.config.js
@@ -0,0 +1,114 @@
+const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+
+module.exports = {
+    entry: {
+        index: './scripts.js'
+    },
+    output: {
+        path: path.resolve(__dirname, `dist`),
+        filename: 'scripts.js',
+        publicPath: `/dist/`
+    },
+    optimization: {
+        minimizer: [
+            new UglifyJsPlugin({
+                cache: true,
+                parallel: true,
+                sourceMap: false
+            }),
+            new OptimizeCSSAssetsPlugin({})
+        ]
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            },
+
+            {
+                // STYLE
+                test: /\.(sa|sc|c)ss|css$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            plugins: [
+                                require('autoprefixer')({
+                                    browsers: ['> 0%'],
+                                    cascade: false
+                                }),
+                            ]
+                        }
+                    },
+                    'sass-loader'
+                ]
+            },
+            // {
+            //     // DATA IMAGES
+            //     test: /\.(png|jpe?g|svg)$/,
+            //     use: [
+            //         {
+            //             loader: 'url-loader',
+            //             options: {
+            //                 limit: 8192
+            //             }
+            //         }
+            //     ],
+            //     include: [
+            //         path.resolve(__dirname, `src/icons`)
+            //     ],
+            // },
+            {
+                // IMAGES
+                test: /\.(png|jpe?g|svg)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: 'img/[name].[ext]'
+                        }
+                    },
+                    {
+                        loader: 'image-webpack-loader',
+                        options: {
+                            mozjpeg: {
+                                progressive: true,
+                                quality: 85
+                            },
+                            // optipng.enabled: false will disable optipng
+                            optipng: {
+                                enabled: false,
+                            },
+                            pngquant: {
+                                quality: '80-90',
+                                speed: 4
+                            },
+                            gifsicle: {
+                                interlaced: false,
+                            },
+                            // the webp option will enable WEBP
+                            // webp: {
+                            //     quality: 75
+                            // }
+                        }
+                    }
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: "[name].css",
+            chunkFilename: "[id].css"
+        })
+    ]
+};
