7eb605a8e60504f0a49cc18bd2d5e32940890673
Vladislav Manakov <footzi@mail.ru>
2018-10-31 10:38:41 +0300

    Добавляет purifycss

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ const gulp = require("gulp");
 const sass = require("gulp-sass");
 const concat = require("gulp-concat");
 const uglify = require("gulp-uglify-es").default;
+const purify = require("gulp-purifycss");
 
 // Styles
 gulp.task("styles", function() {
@@ -12,6 +13,14 @@ gulp.task("styles", function() {
         .pipe(gulp.dest("./"));
 });
 
+//PurifyCss
+gulp.task("purify", function() {
+    return gulp
+        .src("./styles.css")
+        .pipe(purify(["./scripts.js", "./index.html"]))
+        .pipe(gulp.dest("./"));
+});
+
 //Scripts
 gulp.task("scripts", function() {
     gulp.src("scripts/scripts.js")
@@ -19,6 +28,6 @@ gulp.task("scripts", function() {
         .pipe(gulp.dest("./"));
 });
 
-const buildTasks = ["styles", "scripts"];
+const buildTasks = ["styles", "purify", "scripts"];
 
 gulp.task("build", buildTasks);
