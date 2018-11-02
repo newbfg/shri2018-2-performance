e2831093bec2aebc4d2f1001f2d6dbff910c01b3
Nikolai <nikolai.laevskii@yandex.ru>
2018-11-02 05:13:42 +0300

    убран banner.js и заменен статикой

--- banner.js
+++
@@ -1,1 +0,0 @@
-document.write("<a target=_blank href=\"http://ya.ru\"><img src=\"assets/banner.jpg\" border=0></a>");if(0 == 1){document.write('<scr' + 'ipt type="text/javascript" src="HTTP://about:blank.js?plugin="></scr' + 'ipt><nosc' + 'ript>Green KEY!</no' + 'script>');}
